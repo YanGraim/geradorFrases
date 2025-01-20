3281829c6618b1a07d3462316bb4219a497d0238
YanGraim <yangraim@gmail.com>
2025-01-20 16:53:25 -0300

    estruturando a pagina com tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import logoimg from './assets/logo.png'
 
 function App() {
 
   return (
-    <div>
-      <h1>teste</h1>
+    <div className='container'>
+      <img src={logoimg} alt="Logo frases" className='logo'/>
+
+      <h2 className='title'>Categorias</h2>
+      <section className='category-area'>
+        <button className='category-button'>Motivação</button>
+        <button className='category-button'>Bem estar</button>
+      </section>
+      <button className='button-frase'>Gerar Frase</button>
+
+      <h3 className='textoFrase'>Alguma frase vai vir aqui</h3>
     </div>
   )
 }
